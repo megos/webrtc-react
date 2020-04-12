b840ce82e6aa0f03786d278cde147b73cec94502
megos <gmegos.27@gmail.com>
2020-04-12 11:35:57 +0900

    Remove peer state

--- src/usePeer.ts
+++ src/usePeer.ts
@@ -1,16 +1,14 @@
 import Peer from 'skyway-js'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 
 export function usePeer() {
   const ref = useRef<Peer | null>(null)
-  const [peer, setPeer] = useState<Peer | null>(null)
   useEffect(() => {
     ref.current = new Peer({
         key: process.env.REACT_APP_SKYWAY_KEY ?? 'PLEASE SET SKYWAY_KEY',
         debug: 3,
       })
-    setPeer(ref.current)
     return () => ref.current?.destroy()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
-  return peer
+  return ref.current
 }
