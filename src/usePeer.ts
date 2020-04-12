f60343aaa144ceac8ac5dd5888e711e12a89f76d
megos <gmegos.27@gmail.com>
2020-04-12 12:00:11 +0900

    Apply prettier

--- src/usePeer.ts
+++ src/usePeer.ts
@@ -5,9 +5,9 @@ export function usePeer() {
   const ref = useRef<Peer | null>(null)
   useEffect(() => {
     ref.current = new Peer({
-        key: process.env.REACT_APP_SKYWAY_KEY ?? 'PLEASE SET SKYWAY_KEY',
-        debug: 3,
-      })
+      key: process.env.REACT_APP_SKYWAY_KEY ?? 'PLEASE SET SKYWAY_KEY',
+      debug: 3,
+    })
     return () => ref.current?.destroy()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
   return ref.current
