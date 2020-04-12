235283b20914c4ed177ec77c073a47379fcd9a5c
megos <gmegos.27@gmail.com>
2020-04-12 12:02:49 +0900

    Add useCreatePeer

--- src/usePeer.ts
+++ src/usePeer.ts
@@ -6,9 +6,21 @@ export function usePeer() {
   useEffect(() => {
     ref.current = new Peer({
       key: process.env.REACT_APP_SKYWAY_KEY ?? 'PLEASE SET SKYWAY_KEY',
-      debug: 3,
+      debug: process.env.NODE_ENV === 'development' ? 3 : 0,
     })
     return () => ref.current?.destroy()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [])
   return ref.current
+}
+
+export function useCreatePeer() {
+  const ref = useRef<Peer | null>(null)
+  useEffect(() => () => ref.current?.destroy(), [])
+  return (id: string) => {
+    ref.current = new Peer(id, {
+      key: process.env.SKYWAY_KEY ?? 'PLEASE SET SKYWAY_KEY',
+      debug: process.env.NODE_ENV === 'development' ? 3 : 0,
+    })
+    return ref.current
+  }
 }
